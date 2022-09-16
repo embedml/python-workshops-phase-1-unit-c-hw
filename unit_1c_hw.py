3f92464c6906ae439d4315e07a7b8c7033374ac4
Caleb Wheeler <cbwheele@ncsu.edu>
2022-09-16 15:10:00 -0400

    feat: Lots of suggested changes

--- unit_1c_hw.py
+++ unit_1c_hw.py
@@ -13,46 +13,46 @@ def assign_1():
     ''' Assign a variable x the integer 5 '''
     x = None
     # Put your code below#
-
+    
     ######################
     return x # This is what will be set to the test for grading
 
-def assign_test_2():
+def assign_2():
     '''Assign the variable x the string "Hello NC State"'''
     x = None
     # Put your code below#
-
+    
     ######################
     return x
 
 ############ Lists ############
 
-def list_test_1():
+def list_1():
     '''Assign X as a list with the values 3, 4, and 5'''
     x = None
     # Put your code below #
-
+    
     ######################
     return x
 
-def list_test_2():
+def list_2():
     '''Given the list x, use indexing to change the value 3 to the value 7'''
     x = [4, 3, 9]
     # Put your code below #
-
+    
     ######################
     return x
 
-def list_test_3():
+def list_3():
     '''Given the list x, use indexing to change the value 9 to the value 7'''
     x = [4, 3, 9]
     # Put your code below #
-
+    
     ######################
     return x
 
-def list_test_4():
-    '''Given the list x, and list y, assign indices of list x 2 - 11, the values of list y! Hint: Use the row operator for lists!
+def list_4():
+    '''Given the list x, and list y, assign the indices 2 through 11 of the list x the values of list y! Hint: Use the row operator for lists!
     Don't forget that Python does not include the last digit of the right most number.
 
     For example, if we have a list b = [0, 1, 2, 3], if we wanted the 0-2 index we must specify b[0:3] (Note, see how the right number is one more greater than the end index we want)
@@ -60,22 +60,22 @@ def list_test_4():
     x = [0, 0 , 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
     y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     # Put your code below #
-
+    
     ######################
     return x
 
 ######### Dictionaries #########
 
-def dict_test_1():
+def dict_1():
     '''Assign X as a dict with the keys 0, 1, 2, and values "A", "B", "C", respectively '''
     x = 0
     # Put your code below#
-
+    
     ######################
     return x
 
-def dict_test_2():
-    '''Given dict animal_noises, assign y the value at the key "cat" '''
+def dict_2():
+    '''Given dict animal_noises, assign x the value at the key "cat" '''
     animal_noises = {
         "dog" : "bark",
         "cow" : "moo",
@@ -84,10 +84,11 @@ def dict_test_2():
     }
     x = ''
     # Put your code below#
+    
     ######################
     return x
 
-def dict_test_3():
+def dict_3():
     '''Given dict inventory, assign x the amount of tax for milk '''
     inventory = {
         "butter" : {
@@ -105,29 +106,29 @@ def dict_test_3():
     }
     x = ''
     # Put your code below#
-
+    
     ######################
     return x 
 
-def list_dict_test():
+def list_dict():
     '''
-    Assign a dict with keys "warm_colors" and "cool_colors". 
+    Give the dictionary colors the keys "warm_colors" and "cool_colors". 
     Give the warm_colors key a value of a list with the values yellow, orange, red.
     Give the cool_colors key a value of a list with the values, green, blue, purple.
 
-    Note it is case sensitive! 
+    Note: this is case sensitive! 
     '''
     colors = {}
     
     # Put your code below#
-
+    
     ######################
 
     return colors 
 
 ######## if, elif, else ########
 
-def if_test(x):
+def if_practice(x):
     '''
     x is going to be a random number, use if, elif and else to return a value to the tester. 
     
@@ -138,20 +139,21 @@ def if_test(x):
     # Note: You will have to indent the return statements onces you create your if, elif and else statements
 
     # Put/alter your code below#
-
+    
     return "X is 5!"
-
+    
     return "X is 6!"
-
+    
     return "X is not 5 or 6!"
     ######################
 
 
 ########## for loops ###########
 
-def for_loop_test():
+def for_loop():
     '''
-    A list with 400 elements will be given, for each 1 replaces the value with a 2
+    A list with 400 elements will be given.
+    For each entry with a value of 1, replace it with a value of 2
 
     Hint: Iterate through all elements in the list, then check if the value is a 1, if it is a one use that index of the list
     to write the value as a 2. 
@@ -164,22 +166,27 @@ def for_loop_test():
     x = [0, 1, 0, 3] * 400 # List with pattern [0, 1, 0, 3, 0, 1, 0, 3, ...  3]
     index = 0
     # Put/alter your code below#
-
+    
     ######################
     return x
 
 
 ########## while loops #########
 
-def while_loop_test():
+def while_loop():
     '''Create a while loop that will only break if the expression for the while loop evaluates to the number 5'''
     x = None
-    # Have the while loop have x == 5
-    # Then use the code below to generate a new number for x
-
-    x = random.randint(0, 10)# This should live in the while loop, so you may need to indent it once you have the while loop implemented
-    # Note on random.randint(). This will generate an integer between the two values passed in each time it is ran.
-
+    '''
+    The loop's condition should have the loop stop when the value of x is equal to 5.
+    Then, inside the while loop use the code below to generate a new number for x on every single iteration of the loop
+        x = random.randint(0, 10) # This should live in the while loop, so you may need to indent it once you have the while loop implemented
+    (Note on random.randint(). This will generate an integer between the two values passed in each time it is run.)
+    '''
+    # Put/alter your code below#
+    
+    x = random.randint(0, 10) # This should live in the while loop, so you may need to indent it once you have the while loop implemented
+    
+    ######################
     return x
 
 
